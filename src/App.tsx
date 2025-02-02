6b1439b20f679a03e62900fe32b0b7f49e559666
Adeoluwa Gbenro <joelayomide35@gmail.com>
2025-02-02 23:57:21 +0100

    img

--- src/App.tsx
+++ src/App.tsx
@@ -23,19 +23,21 @@ function App() {
   }, []);
 
   return (
-    <div className="w-screen h-screen bg-black flex items-center justify-center relative">
+    <div className="w-screen h-screen bg-black/90 flex items-center justify-center relative">
       {/* Background Audio */}
-      <audio ref={audioRef} src="/audio/game-intro.mp3" loop />
-
+      <audio ref={audioRef} src="/homesound.m4a" loop />
+   {/* Gradient Overlay */}
+      {/* <div className="absolute inset-0 bg-gradient-to-b from-black/80 to-transparent"></div> */}
       {/* Welcome Screen */}
       {!showHome ? (
         <motion.div
           initial={{ scale: 1 }}
           animate={{ scale: 0.1, opacity: 0 }}
           transition={{ duration: 2, ease: "easeInOut" }}
-          className="text-white text-4xl md:text-6xl font-bold"
+          className="text-white text-4xl md:text-6xl font-bold main_text text-center"
         >
-          Ekemuku <span className="text-red-500">(Do not talk about it)</span>
+         <h1 className="text-white text-4xl md:text-[120px] font-bold main_text">Ekemuku</h1> 
+          <span className="text-gray-500 side_text text-[60px]">Do not talk about it</span>
         </motion.div>
       ) : (
         // Home Page
@@ -45,11 +47,14 @@ function App() {
           transition={{ duration: 1.5, ease: "easeOut" }}
           className="w-full h-full flex flex-col items-center justify-center text-white text-3xl"
         >
-          <h1 className="text-5xl font-bold">Welcome to the Game</h1>
-          <p className="mt-4 text-lg">Get ready for the adventure!</p>
-          <button className="mt-6 px-6 py-3 bg-blue-600 hover:bg-blue-700 rounded-lg">
+          
+           <video src="/HomeVideo.mp4" className="absolute top-0 left-0 w-full h-full object-contain" autoPlay loop></video>
+          <div className="relative z-30">
+          <h1 className="text-5xl font-bold main_text">Welcome to the Game</h1>
+          <p className="mt-4 text-lg side_text">Get ready for the adventure!</p>
+          <button className="mt-6 px-6 py-3 p_text bg-blue-600 hover:bg-blue-700 rounded-lg">
             Start Game
-          </button>
+          </button></div>
         </motion.div>
       )}
     </div>
